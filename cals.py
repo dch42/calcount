9b581fa00841da2d05068b8531932c84a7bfbed7
ddch42 <85430824+dch42@users.noreply.github.com>
2022-05-21 21:09:07 -0700

    cleanup classes

--- cals.py
+++ cals.py
@@ -83,9 +83,27 @@ class Entry:
         """Add item to entry content array"""
         self.content.append(item)
 
+    def validate_entry(self):
+        """Validate and build caloric log entry for addition or removal"""
+        try:
+            cal_entry = [
+                str(self.content[0]),
+                int(self.content[1]),
+                int(self.content[2]),
+                str(time),
+                str(date)
+            ]
+            return cal_entry
+        except ValueError as err:
+            option = 'a' if args.a else 'r'
+            print(
+                f"{ERROR} Usage: cals -{option} 'Protein Bar' 190 16\n\
+                    {err}")
+            sys.exit(1)
+
     def commit_cals(self):
         """Commit caloric intake entry to db"""
-        entry = validate_entry(self.content)
+        entry = self.validate_entry()
         with db:
             create_table('calorie_table')
             cursor.executemany("INSERT INTO calorie_table VALUES (?,?,?,?,?)",
@@ -94,7 +112,7 @@ class Entry:
 
     def remove_cals(self):
         """Remove caloric intake entry from db"""
-        entry = validate_entry(self.content)
+        entry = self.validate_entry()
         with db:
             try:
                 cursor.execute(
@@ -119,9 +137,7 @@ class Entry:
 class Profile:
     """
     A class to represent user profile data for calculations
-
     ...
-
     Attributes
     ----------
     age : int
@@ -139,8 +155,12 @@ class Profile:
     -------
     harris_benedict():
         Calculate BMR using Harris-Benedict equation
-    tdee_to_goal():
+    calc_tdee():
+        Calculate TDEE from BMR
+    calc_goal():
         Calculate caloric goal from TDEE
+    commit_profile():
+        Commit goal info to db
     """
 
     def __init__(self):
@@ -157,30 +177,55 @@ class Profile:
         self.height, self.weight = to_metric(self.height, self.weight)
         self.lose = validate_input(
             "Please enter desired weight loss per week (lbs): ", float)
+        self.bmr = self.harris_benedict()
+        self.tdee = self.calc_tdee()
+        self.goal = self.calc_goal()
 
     def harris_benedict(self):
         """Calculate BMR using Harris-Benedict equation"""
         nums = [88.362, 13.397, 4.799, 5.677] if self.sex == 'm' else [
             447.593, 9.247, 3.098, 4.330]
+        print("\nCalculating basal metabolic rate (BMR)...")
         bmr = (nums[0] + (nums[1]*self.weight) +
                (nums[2]*self.height) - (nums[3]*self.age))
+        print(
+            f"\nResults:\n\n\tBMR: ~{int(bmr)} calories")
         return bmr
 
-    def tdee_to_goal(self):
-        """Calculate caloric goal from TDEE"""
-        print("\nCalculating basal metabolic rate (BMR)...")
-        bmr = self.harris_benedict()
+    def calc_tdee(self):
+        """Calculate TDEE from BMR and activity multiplier"""
+        print("\nAverage Daily Activity Level:\n")
+        # activity levels and multipliers for tdee
+        activity = {
+            '1': ["sedentary (little or no exercise)", 1.2],
+            '2': ["light activity (light exercise/sports 1 to 3 days per week)", 1.375],
+            '3': ["moderate activity (moderate exercise/sports 3 to 5 days per week)", 1.55],
+            '4': ["very active (hard exercise/sports 6 to 7 days per week)", 1.725],
+            '5': ["extra active (very hard exercise/sports 6 to 7 days per week and physical job)", 1.9]
+        }
+        for k in activity:
+            print(k, ": ", activity[k][0])
+        print("\n")
+        multiplier = input(
+            "Please enter the option that most closely resembles your average activity level (1-5): ")
         print("Calculating total daily energy expenditure (TDEE)...")
-        tdee = calc_tdee(bmr)
-        goal = tdee - (self.lose*500)
+        tdee = self.bmr*float(activity[multiplier][1])
         print(
-            f"\nResults:\n\n\tBMR: ~{int(bmr)} calories\n\tTDEE: ~{int(tdee)} calories\n")
+            f"\nResults:\n\n\tTDEE: ~{int(tdee)} calories\n")
+        return tdee
+
+    def calc_goal(self):
+        """Calculate caloric goal from TDEE"""
+        goal = self.tdee - (self.lose*500)
         print(
             f"To lose {self.lose} lbs/week, you will need to consume {int(goal)} calories/day.\
                 \nGood luck!\n")
+
+    def commit_profile(self):
+        """Commit goal info to db"""
         entry = [
             self.lose,
-            goal,
+            self.goal,
             time,
             date
         ]
@@ -348,45 +393,6 @@ def export_cals(db):
         print(f"{ERROR} Export failed: {err}\033[0m")
 
 
-def validate_entry(user_input):
-    """Validate and build caloric log entry for addition or removal"""
-    try:
-        record = [
-            str(user_input[0]),
-            int(user_input[1]),
-            int(user_input[2]),
-            str(time),
-            str(date)
-        ]
-        return record
-    except ValueError as err:
-        option = 'a' if args.a else 'r'
-        print(
-            f"{ERROR} Usage: cals -{option} 'Protein Bar' 190 16\n\
-                {err}")
-        sys.exit(1)
-
-
-def calc_tdee(bmr):
-    """Calculate TDEE from BMR and activity multiplier"""
-    print("\nAverage Daily Activity Level:\n")
-    # activity levels and multipliers for tdee
-    activity = {
-        '1': ["sedentary (little or no exercise)", 1.2],
-        '2': ["light activity (light exercise/sports 1 to 3 days per week)", 1.375],
-        '3': ["moderate activity (moderate exercise/sports 3 to 5 days per week)", 1.55],
-        '4': ["very active (hard exercise/sports 6 to 7 days per week)", 1.725],
-        '5': ["extra active (very hard exercise/sports 6 to 7 days per week and physical job)", 1.9]
-    }
-    for k in activity:
-        print(k, ": ", activity[k][0])
-    print("\n")
-    multiplier = input(
-        "Please enter the option that most closely resembles your average activity level (1-5): ")
-    tdee = bmr*float(activity[multiplier][1])
-    return tdee
-
-
 def logo():
     """Print script logo"""
     pyfiglet.print_figlet("CalCount")
@@ -417,7 +423,7 @@ if __name__ == '__main__':
     if args.init:
         logo()
         record = Profile()
-        record.tdee_to_goal()
+        record.commit_profile()
     if args.a or args.r:
         record = Entry()
         if args.a:
